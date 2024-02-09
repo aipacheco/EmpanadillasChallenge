c7223ecedde999bf938719ec5ddc5bfe39d07233
aipacheco <kornetera@gmail.com>
2024-02-09 20:21:36 +0100

    cambiado uso de let por const

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
-export const fn = (/* argumentos de la función */) => {
-   // Cuerpo de la función: Completa la lógica de la función aquí.
-};
+
+   export const fn = (a, b, c) => {
+      let resultado = 0
+      let costeA = 0
+      let costeB = 0
+      let costeC = 0
+      const total = a + b + c
+      if (a < 0 || b < 0 || c < 0) {
+        throw new Error("No se pueden introducir números negativos")
+      }
+      if (total < 40 && total % 3 === 0) {
+        const oferta = total / 3
+        if (a > 0) costeA = a * 12
+        if (b > 0) costeB = b * 14
+        if (c > 0) costeC = c * 16
+    
+        const totalCoste = costeA + costeB + costeC
+    
+        resultado = (totalCoste * oferta) / total
+        return Math.ceil(resultado)
+      } else {
+        throw new Error("Tienen que ser menos de 40 empanadillas y en múltiplos de 3")
+      }
+    }
+    
+
